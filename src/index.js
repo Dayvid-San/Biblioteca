3da852e814147342776eb850ce2b5243a73d2e69
Dayvid Santana <santana.dayvid@outlook.com>
2022-11-14 18:30:32 -0300

    page test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.end("Let's do it!")
 })
 
+app.get('/computacao', (req, res) => {
+    res.end("Computer")
+})
+
 
 app.listen(PORT, () => {
     console.log('it is working on https://localhost:3000')
