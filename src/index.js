77fb282b67fdfe4c0c4e26b8f0e024679e9fb929
Dayvid Santana <santana.dayvid@outlook.com>
2022-12-12 12:38:55 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 const PORT = process.env.PORT || 3000
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use(cors(  ))
 
 app.use(require('./routes'))
 
